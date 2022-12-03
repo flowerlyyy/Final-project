fb73846cd7f5ebc0fb646025b18bea5730f9d036
flowerlyyy <gullushammadli@gmail.com>
2022-12-03 23:31:39 +0400

    correcting error

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 import { Route, Routes } from 'react-router-dom';
 import { HomePage } from './pages/HomePage/HomePage';
@@ -13,20 +11,6 @@ import { SigninPage } from './pages/SigninPage/SigninPage';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
       <main>
       <Routes>
         <Route path="/" element ={<HomePage/>}/>
