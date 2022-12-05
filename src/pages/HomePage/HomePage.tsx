a773791748318c6b5b5037d58c7a392a656d3d3b
Javid Hatamov <cavidh.ov@mail.ru>
2022-12-05 19:14:05 +0400

    html for Question was moved into Questions component

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,60 +3,7 @@ import './HomePage.scss';
 export const HomePage = () => {
 return(
 <>
-<div className="QuestionBlog narrow-container">
-        <div className="Blog">
-            <div className="Question">How do I place an order ?</div>
-            <div className="Arrow"><button><svg xmlns="http://www.w3.org/2000/svg" width="35" height="16" viewBox="0 0 36 18" fill="none">
-                <line x1="1" y1="9" x2="33" y2="9" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                <path d="M25 17L34.6096 9.31235C34.8097 9.15222 34.8097 8.84778 34.6096 8.68765L25 1" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                </svg></button></div>
-        </div>
-        <div className="Blog">
-            <div className="Question">On which days and at what hours does the delivery take place ?</div>
-            <div className="Arrow"><button><svg xmlns="http://www.w3.org/2000/svg" width="35" height="16" viewBox="0 0 36 18" fill="none">
-                <line x1="1" y1="9" x2="33" y2="9" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                <path d="M25 17L34.6096 9.31235C34.8097 9.15222 34.8097 8.84778 34.6096 8.68765L25 1" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                </svg></button></div>
-        </div>
-        <div className="Blog">
-            <div className="Question">For how long can I order a diet?</div>
-            <div className="Arrow"><button><svg xmlns="http://www.w3.org/2000/svg" width="35" height="16" viewBox="0 0 36 18" fill="none">
-                <line x1="1" y1="9" x2="33" y2="9" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                <path d="M25 17L34.6096 9.31235C34.8097 9.15222 34.8097 8.84778 34.6096 8.68765L25 1" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                </svg></button></div>
-        </div>
-        <div className="Blog">
-            <div className="Question">Can I change the delivery address?</div>
-            <div className="Arrow"><button><svg xmlns="http://www.w3.org/2000/svg" width="35" height="16" viewBox="0 0 36 18" fill="none">
-                <line x1="1" y1="9" x2="33" y2="9" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                <path d="M25 17L34.6096 9.31235C34.8097 9.15222 34.8097 8.84778 34.6096 8.68765L25 1" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                </svg></button></div>
-        </div>
-        <div className="Blog">
-            <div className="Question">Can I suspend catering?</div>
-            <div className="Arrow"><button><svg xmlns="http://www.w3.org/2000/svg" width="35" height="16" viewBox="0 0 36 18" fill="none">
-                <line x1="1" y1="9" x2="33" y2="9" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                <path d="M25 17L34.6096 9.31235C34.8097 9.15222 34.8097 8.84778 34.6096 8.68765L25 1" stroke="#111112" stroke-width="2" stroke-linecap="round"/>
-                </svg></button></div>
-        </div>
-    </div>
-    <div className="strange-thing">
-        <svg xmlns="http://www.w3.org/2000/svg" width="85" height="198" viewBox="0 0 85 198" fill="none" >
-            <line x1="42.5" y1="-2.18557e-08" x2="42.5" y2="161" stroke="#EAEAEA"/>
-            <path d="M0 197.435C34 196.078 42 183.5 42 159H43V198L0 197.435Z" fill="url(#paint0_linear_22_941)"/>
-            <path d="M85 197.435C51 196.078 43 183.5 43 159H42V198L85 197.435Z" fill="url(#paint1_linear_22_941)"/>
-            <defs>
-            <linearGradient id="paint0_linear_22_941" x1="21.5" y1="159" x2="21.5" y2="198" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#EAEAEA"/>
-            <stop offset="1" stop-color="#F5F5F5"/>
-            </linearGradient>
-            <linearGradient id="paint1_linear_22_941" x1="63.5" y1="159" x2="63.5" y2="198" gradientUnits="userSpaceOnUse">
-            <stop stop-color="#EAEAEA"/>
-            <stop offset="1" stop-color="#F5F5F5"/>
-            </linearGradient>
-            </defs>
-            </svg>
-    </div>
+
 </>
 );
 }
