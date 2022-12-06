f3776340d32c315d118932f0cfdc9df5f9ab1dd8
Javid Hatamov <cavidh.ov@mail.ru>
2022-12-07 01:09:26 +0400

    Much more clear

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,10 @@
-import { Questions } from './components/Questions/Questions';
 import './HomePage.scss';
+import { Questions } from './components/Questions/Questions';
 
 export const HomePage = () => {
-return(
-<>
-<Questions/>
-</>
-);
-}
+  return (
+    <>
+      <Questions />
+    </>
+  );
+};
