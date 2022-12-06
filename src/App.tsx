29723048a246b2f782091061d635aca0bfd68564
Javid Hatamov <cavidh.ov@mail.ru>
2022-12-06 17:34:09 +0400

    Footer is added

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,29 @@
 import './App.css';
-import { Route, Routes } from 'react-router-dom';
-import { HomePage } from './pages/HomePage/HomePage';
 import { DeliveryPage } from './pages/DeliveryPage/DeliveryPage';
 import { DietsPage } from './pages/DietsPage/DietsPage';
 import { Footer } from './pages/Footer/Footer';
 import { Header } from './pages/Header/Header';
+import { HomePage } from './pages/HomePage/HomePage';
 import { MenuPage } from './pages/MenuPage/MenuPage';
+import { Route, Routes } from 'react-router-dom';
 import { SigninPage } from './pages/SigninPage/SigninPage';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
+      <Header />
       <main>
-      <Routes>
-        <Route path="/" element ={<HomePage/>}/>
-        <Route path="delivery" element={<DeliveryPage/>}/>
-        <Route path="diets" element={<DietsPage/>}/>
-        <Route path="footer" element={<Footer/>}/>
-        <Route path="header" element={<Header/>}/>
-        <Route path="menu" element={<MenuPage/>}/>
-        <Route path="signin" element={<SigninPage/>}/>
-      </Routes>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="delivery" element={<DeliveryPage />} />
+          <Route path="diets" element={<DietsPage />} />
+          <Route path="footer" element={<Footer />} />
+          <Route path="header" element={<Header />} />
+          <Route path="menu" element={<MenuPage />} />
+          <Route path="signin" element={<SigninPage />} />
+        </Routes>
       </main>
-      <footer>
-
-      </footer>
+      <Footer />
     </div>
   );
 }
