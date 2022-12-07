858a81f22f48ac4ff690343cf516e6bf12543dea
flowerlyyy <gullushammadli@gmail.com>
2022-12-07 21:14:29 +0400

    order is changed

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
-import { Comments } from './components/Comments/Comments';
 import './HomePage.scss';
 import { BoxDiets } from './components/BoxDiets/BoxDiets';
+import { Comments } from './components/Comments/Comments';
 
 export const HomePage = () => {
-return(
-<Comments/>
-)
-  return <BoxDiets />;
+  return (
+    <>
+      <Comments />
+      <BoxDiets />
+    </>
+  );
 };
