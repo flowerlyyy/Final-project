6e166e16e1f22a1014d70de7ac157b4f659c22fa
Anton Efremenkov <anton.efremenkov@itentika.ru>
2022-12-07 17:59:25 +0300

    fix: header and footer restored

--- src/pages/Header/Header.tsx
+++ src/pages/Header/Header.tsx
@@ -1,11 +1,18 @@
-import { NavBar } from "../../Components/NavBar/Navbar";
-import "../Header/Header.scss";
+import { NavBar } from '../../Components/NavBar/Navbar';
+import '../Header/Header.scss';
 import logo from './Logo.png';
 import bag from './Bag.png';
 
-
 export const Header = () => {
-    return(
-        <></>
-    );
+  return (
+    <header className="narrow-container">
+      <img className="logo" src={logo} alt="logo" />
+      <NavBar />
+
+      <div className="bagsign">
+        <img className="bag" src={bag} alt="bag" />
+        <button className="sign">Sign in</button>
+      </div>
+    </header>
+  );
 };
