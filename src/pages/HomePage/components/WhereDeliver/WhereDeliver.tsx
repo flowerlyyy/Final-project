8a1734fb17b89002df045774188eff386ed25545
Babayeva76 <babayevavusale.842@gmail.com>
2022-12-14 17:58:42 +0400

    WhereDeliver problems are fixed

--- src/pages/HomePage/components/WhereDeliver/WhereDeliver.tsx
+++ src/pages/HomePage/components/WhereDeliver/WhereDeliver.tsx
@@ -2,19 +2,18 @@ import './WhereDeliver.scss';
 import line13 from './Line 13.svg';
 import Vector from './Vector.svg';
 
-export const WhereDeliver= () =>{
-    return(
-        <div className="WhereDeliver narrow-container">
-        <div className="line-div">
-        <img src={line13} alt="lineerror"/>
-    </div>
+export const WhereDeliver = () => {
+  return (
+    <div className="where-deliver narrow-container">
+      <div className="line-div">
+        <img src={line13} alt="lineerror" />
+      </div>
       <p className="where">Where we deliver</p>
       <div className="where-parts">
         <div className="where-leftpart">
           <p className="see-if">
-            See if we're delivering to you right now! Use the search engine by
-            entering the postal code of your locality and see if we can deliver
-            your order there.
+            See if we're delivering to you right now! Use the search engine by entering the postal code of
+            your locality and see if we can deliver your order there.
           </p>
           <input className="city-name" type="text" placeholder="City name " />
           <div className="check-locate">
@@ -41,5 +40,5 @@ export const WhereDeliver= () =>{
         </div>
       </div>
     </div>
-    );
-    };
+  );
+};
