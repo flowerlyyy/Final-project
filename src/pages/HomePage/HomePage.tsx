e98d175810e23a062db30ee832046905bc51ac5d
flowerlyyy <gullushammadli@gmail.com>
2022-12-15 00:56:21 +0400

    HeroBlog is added.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -1,10 +1,12 @@
-import { Comments } from './components/Comments/Comments';
 import './HomePage.scss';
+import { Comments } from './components/Comments/Comments';
+import { Fruits } from './components/Fruits/Fruits';
 
 export const HomePage = () => {
-return(
-<>
-<Comments/>
-</>
-);
+  return (
+    <>
+      <Fruits />
+      <Comments />
+    </>
+  );
 };
