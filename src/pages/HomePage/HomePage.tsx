c3ee31a66a543882a1a8ee3938f1e7f5d44cb944
flowerlyyy <gullushammadli@gmail.com>
2022-12-15 12:06:04 +0400

    FruitsBlog is added to HomePage.tsx

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -3,11 +3,13 @@ import './HomePage.scss';
 import { Questions } from './components/Questions/Questions';
 import { BoxDiets } from './components/BoxDiets/BoxDiets';
 import { Comments } from './components/Comments/Comments';
+import { Fruits } from './components/Fruits/Fruits';
 
 export const HomePage = () => {
 
   return (
     <>
+    <Fruits/>
       <PopularDiets />
       <Comments />
       <BoxDiets />
