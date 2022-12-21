cfd4fb003fd2bada5e81e30fff4bb4001937f231
KamilSafaraliyev <116893211+KamilSafaraliyev@users.noreply.github.com>
2022-12-22 01:32:10 +0400

    Some fixes

--- src/pages/HomePage/components/Blog/Blog.tsx
+++ src/pages/HomePage/components/Blog/Blog.tsx
@@ -1,66 +1,90 @@
 import './Blog.scss';
-import img1 from '../Blog/cake.png';
-import img2 from '../Blog/group84.png';
-import img3 from '../Blog/orange.png';
-import img4 from '../Blog/picture3.png';
-import React from 'react';
-export const Blog = () => {
-    return (
-    <section className="section-blog">
-        <div className="blog">
-            <div className="blog_right-part">
-                <h2 className='blog-h2'>Blog</h2>
-                <div>
-                    <img className="blog_picture" src={img3} alt="blogpp" />
-                </div>
-                <h4 className="right_part-h4">
-                    Vitamin C - what has the most vitamin C?
-                </h4>
-                <p className="text-name">Esther Howard</p>
-                <p className="text-p">
-                    Maecenas at bibendum orci, nec gravida elit. Sed viverra in est ac consequat. Nulla facilisi. Donec efficitur malesuada scelerisque. Praesent luctus, nisi eget viverra pulvinar, dolor sem dapibus dolor, vel congue dolor massa vitae dolor. Integer aliquet
-                    orci non...
-                </p>
-            </div>
-            </div>
+import cake from './cake.png';
+import blood from './blood.png';
+import orange from './orange.png';
+import pizza from './pizza.png';
+import React, { useState } from 'react'; 
+interface Post {
+    id: number;
+    title: string;
+    body: string;
+    author: string;
+    blogImage: string; 
+}
 
-            <div className="blog_left-part">
-                <div className="box">
-                    <img className='picture1' src={img2} />
-                    <div className="all_text_box">
-                        <h3>Fast food - why should we avoid it? How does it affect our health?</h3>
-                        <div className="box-p">
-                            <b>Efficitur malesuada scelerisque. Praesent luctus, nisi eget viverra pulvinar, dolor sem dapibus dolor, vel congue dolor massa vitae...</b>
-                            <br/><br/>
-                            <b>Jerome Bell</b>
-                        </div>
+const posts:Post[] = [
+    {
+        id: 1,
+        title: 'Vitamin C - what has the most vitamin C?',
+        body: "Efficitur malesuada scelerisque. Praesent luctus, nisi eget viverra pulvinar, dolor sem dapibus dolor, vel congue dolor massa vitae...",
+        author: "Esther Howard",
+        blogImage: orange
+    },
+    {
+        id: 2,
+        title: "Fast food - why should we avoid it? How does it affect our health?",
+        body: "Efficitur malesuada scelerisque. Praesent luctus, nisi eget viverra pulvinar, dolor sem dapibus dolor, vel congue dolor massa vitae...",
+        author: "Jerome Bell",
+        blogImage: cake
+    },
+    {
+        id: 3,
+        title: "Blood tests - why should they be done regularly?",
+        body: "Efficitur malesuada scelerisque. Praesent luctus, nisi eget viverra pulvinar, dolor sem dapibus dolor, vel congue dolor massa vitae...",
+        author: "Esther Howard",
+        blogImage: blood
+    },
+    {
+        id: 4,
+        title: "Sweets on a diet - can you eat them?",
+        body: "Efficitur malesuada scelerisque. Praesent luctus, nisi eget viverra pulvinar, dolor sem dapibus dolor, vel congue dolor massa vitae...",
+        author: "Courtney Henry",
+        blogImage: pizza
+    }
+];
+export const Blog = () => {
+    const [selectedPostId, setSelectedPostId] = useState<number>(posts[0].id);
+    const selectedPost = posts.find(post => post.id === selectedPostId) as Post;
+    const unselectedPosts = posts.filter(post => post.id !== selectedPostId);
+
+    return (
+        <section className="section-blog">
+            <div className="blog">
+                <div className="blog_right-part">
+                    <h2 className='blog-h2'>Blog</h2>
+                    <div>
+                        <img className="blog_picture" src={selectedPost.blogImage} alt="/" />
                     </div>
+                    <h4 >
+                        {selectedPost.title}
+                    </h4>     
+                    <p className="text-p">
+                        {selectedPost.body}
+                    </p>
+                    <p className="text-name">{selectedPost.author}</p>
                 </div>
-                <div className="box">
-                    <img className="image" src={img4} alt=""/>
-                    <div className="all_text_box">
-                        <h3>Blood tests - why should they be done regularly?</h3>
-                        <div className="box-p">
-                            <b></b>
-                            <br/> <br/> <br/> <br/>
-                            <b>Esther Howard</b>
-                        </div>
-                    </div>
                 </div>
-                <div className="box">
-                    <img className="image" src={img1} alt=""/>
-                    <div className="all_text_box">
-                        <h3>Sweets on a diet - can you eat them?</h3>
-                        <div className="box-p">
-                            <b>Efficitur malesuada scelerisque. Praesent luctus, nisi eget viverra pulvinar, dolor sem dapibus dolor, vel congue dolor massa vitae...</b>
-                            <br/> 
-                            <br/> 
-                            <br/>
-                            <b>Courtney Henry</b>
-                        </div>
-                    </div>
+
+                <div className="blog_left-part">
+                    {
+                        unselectedPosts.map(post => (
+                            <div className="box" onClick={() => {
+                                setSelectedPostId(post.id)
+                            }}>
+                                <img className='picture1' src={post.blogImage} />
+                                <div className="all_text_box">
+                                    <h3>{post.title}</h3>
+                                    <div className="box-p">
+                                        {post.body} 
+                                        <br />
+                                        <br />
+                                        <p className="text-name">{selectedPost.author}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))
+                    }
                 </div>
-            </div>
-    </section>
-);
+        </section>
+    );
 };
--- src/pages/HomePage/components/BoxDiets/BoxDiets.tsx
+++ src/pages/HomePage/components/BoxDiets/BoxDiets.tsx
@@ -5,47 +5,59 @@ import img7 from '../BoxDiets/group93.png';
 import img8 from '../BoxDiets/group94.png';
 import React from 'react';
 
-export const BoxDiets = () => {
+interface DietBoxProp {
+  title: string;
+  img: string;
+  description: string;
+}
+
+const boxes = [
+{
+  title:'Fresh ingredients',
+  img: img5,
+  description: 'Donec in nisl porttitor, blandit augue in, ullamcorper felis',
+},{
+  title: 'Taste',
+  img: img6,
+  description: 'Apibus sed erat nec volutpat. Interdum et malesuada',
+},{ 
+  title: 'Diversity',
+  img: img7,
+  description: 'Proin hendrerit molestie placerat ec ante congue placerat non sed',
+},{
+  title:'Match',
+  img: img8,
+  description:'Vivamus blandit, lorem non venenatis luctus, leo dui volutpat purusquis tinci',
+}
+];
+
+const DietBox = ({title, img, description,}:DietBoxProp) => {
+  return (
+      <div className="box-box">                                         
+      <img className="symbol" src={img} alt="" />
+      <div className="diet-text">
+        <h4>{title}</h4>
+        <p className="diet-p">{description}</p>
+      </div>
+    </div> 
+  )
+}
+
+export const BoxDiets = () => { 
   return (
     <div className="box-diet blog-container">
-      <h2 className='box-diet-h2'>Box diets with which you get more</h2>
-      <div className="boxes">
-        <div className="box-box">
-          <img className="symbol" src={img5} alt="" />
-
-          <div className="diet-text">
-            <h4>Fresh ingredients</h4>
-            <p className="diet-p">Donec in nisl porttitor, blandit augue in, ullamcorper felis</p>
-          </div>
-        </div>
-
-        <div className="box-box">
-          <img className="symbol" src={img6} alt="" />
-          <div className="diet-text">
-            <h4>Taste</h4>
-            <p className="diet-p">Apibus sed erat nec volutpat. Interdum et malesuada</p>
-          </div>
-        </div>
-
-        <div className="box-box">
-          <img className="symbol" src={img7} alt="" />
-          <div className="diet-text">
-            <h4>Diversity</h4>
-            <p className="diet-p">Proin hendrerit molestie placerat ec ante congue placerat non sed</p>
-          </div>
-        </div>
-
-        <div className="box-box">
-          <img className="symbol" src={img8} alt="" />
-          <div className="diet-text">
-            <h4>Match</h4>
-            <p className="diet-p">
-              {' '}
-              Vivamus blandit, lorem non venenatis luctus, leo dui volutpat purusquis tinci
-            </p>
-          </div>
-        </div>
-      </div>
+      <h2 className='box-diet-h2'>Box diets with which you get more</h2>  
+      <div className="boxes">         
+          {
+            boxes.map((box, index)=> (
+              <DietBox 
+               title={box.title}
+               img={box.img}
+               description={box.description}
+                />
+            ))
+          }
+      </div> 
     </div>
   );
 };
